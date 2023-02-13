99574b4f6f40441844d24355aa0283da1df05576
Dylan Claywell <djclaywell@gmail.com>
2023-02-12 21:57:41 -0600

    Removed unnecessary code

--- src/views/TilemapEditorView.tsx
+++ src/views/TilemapEditorView.tsx
@@ -1,22 +1,11 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useContext } from 'react'
 
-import { TilemapType } from '../types/tilemap'
 import { TilemapEditor } from '../components/TilemapEditor'
 import { Tools } from '../components/Tools/Tools'
 import { ToolSection } from '../components/Tools/ToolSection'
 import { Tool } from '../components/Tool'
 import { TilemapEditorSettings } from '../components/TilemapEditor/Settings'
-import { generateMap } from '../utils/generateMap'
 import { EditorContext } from '../contexts/EditorContext'
-
-const initialTilemap: TilemapType = {
-  name: 'Test',
-  tileWidth: 32,
-  tileHeight: 32,
-  width: 10,
-  height: 10,
-  data: generateMap(10, 10),
-}
 
 export function TilemapEditorView() {
   const [
@@ -24,36 +13,8 @@ export function TilemapEditorView() {
     { updateTilemap, updateTilemapSettings },
   ] = useContext(EditorContext)
   const [showSettings, setShowSettings] = React.useState(false)
-  // const [tilemap, setTilemap] = React.useState<TilemapType>(initialTilemap)
 
   const tilemap = layers.find((layer) => layer.id === selectedLayerId)?.tilemap
-
-  // const updateTile = useCallback(
-  //   ({
-  //     tileX,
-  //     tileY,
-  //     tilesetX,
-  //     tilesetY,
-  //     tilesetName,
-  //   }: {
-  //     tileX: number
-  //     tileY: number
-  //     tilesetX: number
-  //     tilesetY: number
-  //     tilesetName: string
-  //   }) => {
-  //     setTilemap((tilemap) => {
-  //       const newTilemap = { ...tilemap }
-  //       newTilemap.data[tileY][tileX] = {
-  //         tilesetName,
-  //         tilesetX,
-  //         tilesetY,
-  //       }
-  //       return newTilemap
-  //     })
-  //   },
-  //   []
-  // )
 
   return (
     <div className="basis-[70vw] h-screen overflow-hidden">
