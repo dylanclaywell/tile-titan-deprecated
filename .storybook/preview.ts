e9b58a6120c6b1cc03186d66761e116a296095b1
Dylan Claywell <djclaywell@gmail.com>
2023-10-28 22:10:43 -0500

    Finished adding Tailwind to Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
+import { withThemeByDataAttribute } from '@storybook/addon-themes'
 import type { Preview } from '@storybook/react'
+import '../src/index.css'
 
 const preview: Preview = {
   parameters: {
@@ -11,5 +13,16 @@ const preview: Preview = {
     },
   },
 }
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-mode',
+  }),
+]
 
 export default preview
